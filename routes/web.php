e061cfc8fcb2b646d47b77c97a94c865d574d48f
John Muchiri <jmuchiri@aviationmanuals.com>
2023-12-25 01:15:33 -0500

    update

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ Route::get('/', fn() => view('welcome'));
 Route::get('/login', function (){
     return redirect('/admin');
 })->name('login');
+
+Route::get('news', \App\Filament\Pages\News::class);
